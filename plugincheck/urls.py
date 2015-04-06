b5dcf962660b2d7b1468ea7b1a03d97559d65713
Ricky Rosario <rickyrosario@gmail.com>
2015-04-06 12:18:47 -0400

    Redirect / to /admin/ for now.

--- plugincheck/urls.py
+++ plugincheck/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 from plugincheck.base.admin import admin_site
 
@@ -14,6 +15,8 @@ admin_site.copy_registry(admin.site)
 
 
 urlpatterns = patterns('',
+    url('^$', RedirectView.as_view(url='/admin/'), name='home'),
+
     url(r'', include('django_browserid.urls')),
     url(r'', include('plugincheck.plugins.urls')),
 
